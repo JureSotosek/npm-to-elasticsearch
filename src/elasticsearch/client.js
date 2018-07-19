e68bbe4b2ae24ce3ef3182605f0b12d82dc6874c
Jure Sotosek <jure.sotosek@gmail.com>
2018-07-19 20:16:32 +0200

    TEMP

--- src/elasticsearch/client.js
+++ src/elasticsearch/client.js
@@ -22,6 +22,7 @@ export default async function getClient() {
     await client.indices
       .get({ index: config.indexName })
       .then(() => {
+        console.log('🗑 Deleting existing index');
         client.indices.delete({ index: config.indexName });
       })
       .catch(() => {});
